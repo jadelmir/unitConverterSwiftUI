e79a4ca589510b355ae67fe5a2e50e3fec98823d
jad el mir <jadelmir>
2020-05-12 01:58:30 -0400

    final and first commit

--- unitConversion/ContentView.swift
+++ unitConversion/ContentView.swift
@@ -9,8 +9,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var temperatureFrom = 0
+    @State private var temperatureTo = 1
+    @State private var fromTmp = ""
+    
+    let Temperatures = ["Celcius","Fahrenheit","Kelvin"]
+    var toTmp :Double {
+//        standard is celcius
+        var celcius:Double = 0.0
+        var FinalAmount:Double = 0.0
+        let amount = Double(fromTmp) ?? 0
+        if temperatureFrom == 1 {
+            celcius = (amount - 32 )*5 / 9
+        }
+        else if temperatureFrom == 2 {
+            celcius =  amount - 273.15
+        }
+        else {celcius = amount}
+        if temperatureTo == 1 {
+            FinalAmount = (celcius / 5 ) * 9 + 32
+        }
+        else if temperatureTo == 2 {
+            FinalAmount =  celcius + 273.15
+        }
+        else {FinalAmount = celcius}
+        return FinalAmount
+    }
     var body: some View {
-        Text("Hello, World!")
+        Form{
+            Section(header:Text("Temperature")){
+                TextField("Amount",text:$fromTmp )
+                Picker("tem",selection: $temperatureFrom){
+                    ForEach(0 ..< self.Temperatures.count){
+                        Text("\(self.Temperatures[$0])")
+                    }
+                }.pickerStyle(SegmentedPickerStyle())
+            }
+            
+            Section(header:Text("Temperature")){
+                Picker("tem",selection: $temperatureTo){
+                    ForEach(0 ..< self.Temperatures.count){
+                        Text("\(self.Temperatures[$0])")
+                    }
+                }.pickerStyle(SegmentedPickerStyle())
+            }
+            Section{
+                Text("\(toTmp)")
+            }
+        }
     }
 }
 
