6d177f8a87c89f3ddab38e25f6de04c7d34e5f97
jad el mir <jadelmir>
2020-05-12 01:34:20 -0400

    Initial Commit

---
+++ unitConversion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  unitConversion
+//
+//  Created by Jad El mir on 5/12/20.
+//  Copyright © 2020 Jad El mir. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
